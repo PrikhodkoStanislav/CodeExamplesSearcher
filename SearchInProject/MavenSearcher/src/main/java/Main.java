0d37dd4b6cdadd8aa9dc42e154189008f653b98c
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-10-01 14:34:42 +0300

    Maven project with unit tests is created.

---
+++ SearchInProject/MavenSearcher/src/main/java/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Created by Станислав on 28.09.2016.
+ */
+public class Main {
+    public static void test1() {
+        String funName = "strcpy";
+        String pathToFile = "../Linux/linux-4.8-rc8/arch/alpha/boot/bootp.c";
+        Searcher searcher = new Searcher();
+        String result = searcher.search(funName, pathToFile);
+        System.out.println(result);
+    }
+
+    public static void test2() {
+        String funName = "strcpy";
+        String pathToFile = "../Linux/linux-4.8-rc8/arch/alpha/boot/";
+        Searcher searcher = new Searcher();
+        String result = searcher.search(funName, pathToFile);
+        System.out.println(result);
+    }
+
+    public static void test3() {
+        String funName = "printk";
+        String pathToFile = "../Linux/linux-4.8-rc8/kernel/power";
+        Searcher searcher = new Searcher();
+        String result = searcher.search(funName, pathToFile);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Input function name: ");
+//        String funName = in.nextLine();
+//        System.out.println("Input path to the file: ");
+//        String pathToFile = in.nextLine();
+
+//        test1();
+//        test2();
+        test3();
+
+
+    }
+}
+
---
+++ SearchInProject/MavenSearcher/src/test/java/SearcherTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Станислав on 01.10.2016.
+ */
+public class SearcherTest {
+
+    Searcher searcher = new Searcher();
+
+    @Test
+    public void testEmptyDirectory() {
+        assertEquals("", searcher.search("someFunction", "../emptyDirectory"));
+    }
+
+    @Test
+    public void testEmptyFile() {
+        assertEquals("", searcher.search("someFunction", "../emptyFile.txt"));
+    }
+
+    @Test
+    public void testFile() {
+        assertEquals("Example 1 : str 1 :\n" +
+                "----------\n" +
+                "theFunction(someArguments)\n" +
+                "----------\n\n", searcher.search("theFunction", "../file.txt"));
+    }
+}
