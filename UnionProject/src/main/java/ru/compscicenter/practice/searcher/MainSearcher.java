219434ae1bb45e83685ed743431f2fadc9294bd4
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-10-05 20:06:57 +0300

    Union Project is born.

---
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/MainSearcher.java
@@ -0,0 +1,10 @@
+package ru.compscicenter.practice.searcher;
+
+/**
+ * Created by Станислав on 05.10.2016.
+ */
+public class MainSearcher {
+    public static void main(String[] args) {
+
+    }
+}
