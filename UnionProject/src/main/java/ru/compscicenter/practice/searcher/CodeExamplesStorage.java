3d1a32d38e273f7cf7ef313a941ed96a82657c1c
roddar92 <rodionowadarja@yandex.ru>
2016-10-13 21:58:25 +0300

    Add schema of code examples storage

---
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/CodeExamplesStorage.java
@@ -0,0 +1,30 @@
+package ru.compscicenter.practice.searcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 13.10.2016!
+ */
+public class CodeExamplesStorage {
+
+    private List<String> examples;
+
+    private static CodeExamplesStorage instance;
+
+    private CodeExamplesStorage() {
+        examples = new ArrayList<>();
+    }
+
+    public static CodeExamplesStorage getInstance() {
+        if (instance == null)
+            instance = new CodeExamplesStorage();
+        return instance;
+    }
+
+    public void addCodeExample(String example) {
+        examples.add(example);
+    }
+
+    public void removeDuplicates() {}
+}
