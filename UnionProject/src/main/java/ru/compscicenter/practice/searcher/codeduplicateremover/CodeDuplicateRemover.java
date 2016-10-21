b913057ba7100cdedfe01d9fbd11253b0d0e687b
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-10-22 01:14:31 +0300

    Code duplicator for files and code examples. Compare from lexer analysis.

--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/codeduplicateremover/CodeDuplicateRemover.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/codeduplicateremover/CodeDuplicateRemover.java
@@ -16,8 +16,38 @@ public class CodeDuplicateRemover {
         this.list = list;
     }
 
-    public void deleteDuplicates(Map<CodeExample, List<Integer>> tokenTypes) {
+    public boolean compareFunctionsFromFiles(String fileName1, String fileName2) throws Exception {
+        Lexer lexer1 = new CLexer(new ANTLRFileStream(fileName1));
+        Lexer lexer2 = new CLexer(new ANTLRFileStream(fileName2));
 
+        return lexerAnalysis(lexer1, lexer2);
+    }
+
+    public boolean compareCodeExamples(CodeExample codeExample1, CodeExample codeExample2) {
+        Lexer lexer1 = new CLexer(new ANTLRInputStream(codeExample1.codeExample));
+        Lexer lexer2 = new CLexer(new ANTLRInputStream(codeExample2.codeExample));
+
+        return lexerAnalysis(lexer1, lexer2);
+    }
+
+    private boolean lexerAnalysis(Lexer lexer1, Lexer lexer2) {
+        List<? extends Token> str1 = lexer1.getAllTokens();
+        List<? extends Token> str2 = lexer2.getAllTokens();
+
+        List<Integer> arr1 = new ArrayList<>();
+        List<Integer> arr2 = new ArrayList<>();
+
+        for (Token s : str1) {
+            arr1.add(s.getType());
+        }
+        for (Token s : str2) {
+            arr2.add(s.getType());
+        }
+        boolean result = arr1.equals(arr2);
+        return result;
+    }
+
+    public void deleteDuplicates(Map<CodeExample, List<Integer>> tokenTypes) {
     }
 
     public List<CodeExample> removeDuplicates() {
