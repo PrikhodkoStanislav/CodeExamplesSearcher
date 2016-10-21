122836d9cb4e8cd27a3d507cb2033e4378a0fafe
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-10-22 01:13:25 +0300

    Add new examples, Gold File and test system.

---
+++ UnionProject/src/test/java/ru/compscicenter/practice/searcher/testsystem/TestSystem.java
@@ -0,0 +1,16 @@
+package ru.compscicenter.practice.searcher.testsystem;
+
+import org.junit.Test;
+
+/**
+ * Created by Станислав on 22.10.2016.
+ */
+public class TestSystem {
+    private double errorFirstType;
+    private double errorSecondType;
+
+    @Test
+    public void testCodeDuplicateRemover() {
+        
+    }
+}
