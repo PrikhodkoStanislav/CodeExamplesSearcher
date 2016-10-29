5f90c75b3b9f939e59a2f7835d30e75e48a132ee
Evgeny Linsky <evgeny.linsky@cloubear.ru>
2016-10-29 18:11:40 +0300

    scripts for compiling&running under Linux

--- UnionProject/run.sh
+++ UnionProject/run.sh
@@ -1,1 +1,1 @@
-java -cp lib/antlr-4.5.3-complete.jar:lib/antlrclasses.jar:target/searcher-1.0-SNAPSHOT-jar-with-dependencies.jar ru.compscicenter.practice.searcher.MainSearcher --help
+java -cp lib/antlr-4.5.3-complete.jar:lib/antlrclasses.jar:target/searcher-1.0-SNAPSHOT-jar-with-dependencies.jar:lib/org.eclipse.cdt.core_6.0.0.201606062011.jar ru.compscicenter.practice.searcher.MainSearcher $1 $2 $3
