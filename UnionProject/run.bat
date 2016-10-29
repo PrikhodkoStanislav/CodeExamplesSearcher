33c3369aad0bc14a946262363c10446d97581473
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-10-29 13:04:50 +0300

    Jars antlr in directory lib.

--- UnionProject/run.bat
+++ UnionProject/run.bat
@@ -1,1 +1,1 @@
-java -cp antlr-4.5.3-complete.jar;antlrclasses.jar;target/searcher-1.0-SNAPSHOT-jar-with-dependencies.jar ru.compscicenter.practice.searcher.MainSearcher --help
+java -cp lib/antlr-4.5.3-complete.jar;lib/antlrclasses.jar;target/searcher-1.0-SNAPSHOT-jar-with-dependencies.jar ru.compscicenter.practice.searcher.MainSearcher --help
