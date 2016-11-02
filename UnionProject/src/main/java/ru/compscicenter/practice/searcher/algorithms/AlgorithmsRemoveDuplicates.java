87b86350967ed434f8c4760ac31b01164552f717
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-02 18:20:29 +0300

    Add enum.

---
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/algorithms/AlgorithmsRemoveDuplicates.java
@@ -0,0 +1,8 @@
+package ru.compscicenter.practice.searcher.algorithms;
+
+/**
+ * Created by Станислав on 02.11.2016.
+ */
+public enum AlgorithmsRemoveDuplicates {
+    EqualsTokens, LevenshteinDistance
+}
