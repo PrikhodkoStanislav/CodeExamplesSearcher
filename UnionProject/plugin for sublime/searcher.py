df7620397d2e689322144cb87d18685df8351279
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-12 01:49:13 +0300

    Files for Sublime plugin. Keyboard: Ctrl+s+d

---
+++ UnionProject/plugin for sublime/searcher.py
@@ -0,0 +1,28 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), "requests-2.11.1"))
+
+import requests
+import sublime
+import sublime_plugin
+
+class SearchCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		start_region = self.view.sel()[0]
+		cursor = self.view.rowcol(start_region.begin())[0]
+		line = self.view.line(start_region)
+		string = self.view.substr(line)
+		funcName = ""
+		buf = ""
+		for c in string:
+			if c == ' ':
+				buf = ""
+			if c == '(':
+				if (buf == "if") or (buf == "for") or (buf == "while"):
+					buf = ""
+				else:
+					funcName = buf
+					break
+			buf += c
+		url = "http://localhost:8080/set_example?func=%s" % funcName
+		requests.get(url)
