be592ae25e3b2363aa1b42ef7c5fc53d071f61be
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-14 18:56:45 +0300

    Add logger into the server.

--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/CreateServer.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/CreateServer.java
@@ -1,12 +1,19 @@
 package ru.compscicenter.practice.searcher.server;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
+import ru.compscicenter.practice.searcher.selfprojectsearcher.SelfProjectSearcher;
 
 /**
  * Created by Станислав on 09.11.2016.
  */
 public class CreateServer {
+    private final static Logger logger = Logger.getLogger(SelfProjectSearcher.class);
+
     public static void startServer() {
+        logger.setLevel(Level.INFO);
+
         Server server = new Server(8080);
         server.setHandler(new ServerHandler());
         try {
--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/ServerHandler.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/ServerHandler.java
@@ -8,18 +8,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import ru.compscicenter.practice.searcher.MainSearcher;
+import ru.compscicenter.practice.searcher.selfprojectsearcher.SelfProjectSearcher;
 
 /**
  * Created by Станислав on 08.11.2016.
  */
 public class ServerHandler extends AbstractHandler {
+    private final static Logger logger = Logger.getLogger(SelfProjectSearcher.class);
+
     private String result = "<h1>Welcome to the Code Examples Searcher Server!</h1>";
 
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        logger.setLevel(Level.INFO);
+
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
