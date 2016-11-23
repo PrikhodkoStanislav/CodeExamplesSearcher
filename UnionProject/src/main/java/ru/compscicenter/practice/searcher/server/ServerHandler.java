1710078ea7b8fcc5e7d1ac8befb1f35ebd0c7ea8
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-23 20:58:45 +0300

    Setting have fields in html and button submit on click make timeout equals 10000 ms.

--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/ServerHandler.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/server/ServerHandler.java
@@ -12,7 +12,6 @@ import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import ru.compscicenter.practice.searcher.MainSearcher;
-import ru.compscicenter.practice.searcher.selfprojectsearcher.SelfProjectSearcher;
 
 /**
  * Created by Станислав on 08.11.2016.
@@ -25,26 +24,38 @@ public class ServerHandler extends AbstractHandler {
             + "<a href=\"http://localhost:8080/settings\">Settings for searcher</a>";
 
     private String settingsResult = "<h1>Input settings:</h1>"
-            + "<input type=\"text\" name=\"path\" value=\"\" size=\"30\">"
+            + "<p>Input path to the directory for search:</p>"
+            + "<p><input type=\"text\" id=\"path\" name=\"path\" value=\"%1$s\" size=\"50\"></p>"
+            + "<p>Input timout for database updating:</p>"
+            + "<p><input type=\"number\" id=\"timeout\" name=\"timeout\" value=\"%2$d\" min=\"0\"" +
+            "max=\"5000000000\" step=\"1\"></p>"
+            + "<p><input type=\"button\" id=\"button\" onclick=\"f_click();\"" +
+            "value=\"Submit\"></p>"
+            + "<script>"
+            + "function f_click() {"
+            + "document.getElementById(\"path\").value;"
+            + "document.getElementById(\"timeout\").value;"
+            + "document.getElementById(\"timeout\").value = 10000;"
+            + "}"
+            + "</script>"
             + "<a href=\"http://localhost:8080/get_examples\">Page with examples</a>";
 
     private Preferences prefs = Preferences.userRoot().node("settings");
 
-    public void setPreferences(String funcName, String path, String format, long timeStamp) {
-        String ID1 = "functionName";
-        String ID2 = "path";
-        String ID3 = "format";
-        String ID4 = "timeStamp";
+    public void setPreferences(String path, long timeout) {
+        String ID1 = "path";
+        String ID2 = "timeout";
 
-        prefs.put(ID1, funcName);
-        prefs.put(ID2, path);
-        prefs.put(ID3, format);
-        prefs.putLong(ID4, timeStamp);
+        prefs.put(ID1, path);
+        prefs.putLong(ID2, timeout);
     }
 
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         logger.setLevel(Level.INFO);
+
+        final String defaultPath = "./";
+        final long defaultTimeout = 10000;
 
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
@@ -56,10 +67,8 @@ public class ServerHandler extends AbstractHandler {
             String lineStr = request.getParameter("line");
             String string = request.getParameter("string");
             int line = Integer.parseInt(lineStr);
-            String pathForSearch = prefs.get("path", "./");
-            long defaultTimeStamp = 10000;
-            long timeStamp = prefs.getLong("timeStamp", defaultTimeStamp);
-            setPreferences(funcName, pathForSearch, "html", timeStamp);
+            String pathForSearch = prefs.get("path", defaultPath);
+            long timeout = prefs.getLong("timeout", defaultTimeout);
             try {
                 result = MainSearcher.searchExamplesForClient(funcName, pathForSearch, pathFromSublime, line, string);
             } catch (ParseException e) {
@@ -70,9 +79,14 @@ public class ServerHandler extends AbstractHandler {
             response.setContentLengthLong(length);
             response.getWriter().println(result);
         } else if (uri.equals("/settings")) {
-            long length = settingsResult.length();
+            String pathForSearch = prefs.get("path", defaultPath);
+            long timeout = prefs.getLong("timeout", defaultTimeout);
+            String result = String.format(settingsResult, pathForSearch, timeout);
+//            setPreferences(pathForSearch, timeout);
+
+            long length = result.length();
             response.setContentLengthLong(length);
-            response.getWriter().println(settingsResult);
+            response.getWriter().println(result);
         } else if (uri.equals("/")) {
             String result = "<h1>Welcome!</h1>";
             result += "<p>You should go to the page to get examples.</p>";
