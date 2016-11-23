922043f4275b834378f68b6ec0882859312f450c
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-23 19:11:57 +0300

    More clean code in plugin.

--- UnionProject/plugin_for_sublime/searcher.py
+++ UnionProject/plugin_for_sublime/searcher.py
@@ -14,14 +14,19 @@ class SearchCommand(sublime_plugin.TextCommand):
 		string = self.view.substr(line)
 		funcName = ""
 		buf = ""
+		separators = [' ', '\t', '=', '+', '-', '*', '/', '&', '|']
+		constructions = ["if", "for", "while"]
 		for c in string:
-			if (c == ' ') or (c == '\t') or (c == '=') or (c == '+') or (c == '-') or (c == '*') or (c == '/') or (c == '&') or (c == '|'):
-				buf = ""
-				continue
-			if (c == '('):
-				if (buf == "if") or (buf == "for") or (buf == "while"):
+			if (c in separators):
 					buf = ""
 					continue
+			if (c == '('):
+				if (buf in constructions):
+					buf = ""
+					continue
+				if (buf in separators):
+						buf = ""
+						continue
 				else:
 					funcName = buf
 					break
@@ -31,4 +36,3 @@ class SearchCommand(sublime_plugin.TextCommand):
 		url = "http://localhost:8080/set_example?func=%s&path=%s&line=%s&string=%s" % (funcName, path, lineNumber, string)
 		print(url)
 		requests.get(url)
-		
