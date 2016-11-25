da5f8e4baa7ace58cd6a5eaf12bb857610d7e446
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2016-11-25 23:28:26 +0300

    Added offset in formatter.

--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/codeformatter/HandwrittenCodeFormatter.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/codeformatter/HandwrittenCodeFormatter.java
@@ -5,7 +5,25 @@ package ru.compscicenter.practice.searcher.codeformatter;
  */
 public class HandwrittenCodeFormatter {
     public static String format(String sourceString) {
-        String result = sourceString;
+        String result = "";
+        int offset = 1;
+        char[] chars = sourceString.toCharArray();
+        int index = 0;
+        while (index < chars.length) {
+            char c = chars[index];
+            result += c;
+            if (c == '{') {
+                offset *= 4;
+                for (int i = 0; i < offset; i++) {
+                    result += ' ';
+                }
+            } else if (c == '}') {
+                offset /= 4;
+                for (int i = 0; i < offset; i++) {
+                    result += ' ';
+                }
+            }
+        }
         return result;
     }
 }
