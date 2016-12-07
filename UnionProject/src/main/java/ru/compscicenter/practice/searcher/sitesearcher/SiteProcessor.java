56792fa24419589b1ece6ac3122ad07e5c1510a1
roddar92 <rodionowadarja@yandex.ru>
2016-12-07 20:40:24 +0300

    Refactoring method for check natural language

--- UnionProject/src/main/java/ru/compscicenter/practice/searcher/sitesearcher/SiteProcessor.java
+++ UnionProject/src/main/java/ru/compscicenter/practice/searcher/sitesearcher/SiteProcessor.java
@@ -215,8 +215,15 @@ public abstract class SiteProcessor extends Thread {
 
     private boolean isNatural(String token) {
         return !(token.matches("[\\+\\-\\=\\(\\)\\<\\>\\{\\}]") ||
-                token.matches("(int|new|char|float|byte|short|double|const|void|for|if|while|switch)") ||
-                token.length() < 5 || token.contains("_") || isCamelCase(token)/* || token.contains("[\+-\=\(\)\<\>\{\}\[\];]")*/);
+            token.matches("(int|new|null|NULL|else|nullptr|str|var|char|float|byte|short|double|const|void|for|if|while|switch)") ||
+            token.length() < 5 || token.contains("_") || isCamelCase(token) ||
+            token.contains("+") || token.contains("-") ||
+            token.contains("*") || token.contains("/") ||
+            token.contains("=") || token.contains(";") ||
+                token.contains("(") || token.contains(")") ||
+                token.contains("<") || token.contains(">") ||
+                token.contains("{") || token.contains("}") ||
+                token.contains("[") || token.contains("]"));
     }
 
     private boolean isCamelCase(String token) {
